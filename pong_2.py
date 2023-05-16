4a44b9af0c2e8577a2f1052362a5cd2b4f867a50
fancop <113712008+fancop@users.noreply.github.com>
2023-05-16 19:45:16 +0300

    Add files via upload

--- pong_2.py
+++ pong_2.py
@@ -33,16 +33,29 @@ class Game:
             keys=(pygame.K_UP, pygame.K_DOWN),
             is_automatic=True
         )
-        self.ball = Ball(
+        ball = Ball(
             self.rect,
             self.rect.center,
             (self.rect.width * 0.005, self.rect.width * 0.005),
         )
+        score_1 = Score(
+            center=(self.rect.width * 0.25, self.rect.height * 0.1),
+            paddle=player_1
+        )
+        score_2 = Score(
+            center=(self.rect.width * 0.75, self.rect.height * 0.1),
+            paddle=player_2
+        )
 
-        self.all_sprites = pygame.sprite.Group()
-        self.all_sprites.add(player_1)
-        self.all_sprites.add(player_2)
-        self.all_sprites.add(self.ball)
+        self.paddles = pygame.sprite.Group()
+        self.balls = pygame.sprite.Group()
+        self.scores = pygame.sprite.Group()
+        self.paddles.add(player_1)
+        self.paddles.add(player_2)
+        ball.throw_in()
+        self.balls.add(ball)
+        self.scores.add(score_1)
+        self.scores.add(score_2)
         self.clock = pygame.time.Clock()
         self.main_loop()
 
@@ -58,8 +71,11 @@ class Game:
                 game = False
 
             self.screen.fill((BLACK))
-            self.all_sprites.update()
-            self.all_sprites.draw(self.screen)
+            self.paddles.update()
+            self.balls.update()
+            self.paddles.draw(self.screen)
+            self.balls.draw(self.screen)
+            self.scores.draw(self.screen)
             pygame.draw.line(self.screen,
                 WHITE,
                 (self.rect.centerx, self.rect.bottom),
@@ -80,6 +96,7 @@ class Paddle(pygame.sprite.Sprite):
             keys=(pygame.K_UP, pygame.K_DOWN),
             is_automatic=False,
             speed=5,
+            score=0,
     ):
         super().__init__()
         self.image = pygame.Surface((size))
@@ -90,6 +107,7 @@ class Paddle(pygame.sprite.Sprite):
         self.is_automatic = is_automatic
         self.speed = speed
         self.screen_rect = screen_rect
+        self.score = score
     
     def update(self):
         keys = pygame.key.get_pressed()
@@ -126,33 +144,69 @@ class Ball(pygame.sprite.Sprite):
         self.velocity_y = velocity_y
         self.direction = direction
         self.speed = speed
+        self.screen_rect = screen_rect
     
     def update(self):
+        self.move()
+        self.bounce()
+        self.check_goal()
+
+    def move(self):
         self.velocity_x = sin(radians(self.direction)) * self.speed
         self.velocity_y = cos(radians(self.direction)) * self.speed * -1
         self.rect.x += self.velocity_x
         self.rect.y += self.velocity_y
 
     def bounce(self):
-        """
-        отскок мяча от верхней и нижней граници
-        как поменять направление?
-        """
-        pass
+        if self.rect.top <= self.screen_rect.top:
+            self.direction *= -1
+            self.direction += 180
+        elif self.rect.bottom >= self.screen_rect.bottom:
+            self.direction *= -1
+            self.direction += 180
 
-    def goal(self):
+    def throw_in(self):
+        """
+        центрирует мяч и запускает в направлении ворот левого и правого игрока
+        """
+        self.rect.center = self.screen_rect.center
+        if randint(0, 1):
+            self.direction = randint(45, 135)
+        else:
+            self.direction = randint(225, 315)
+
+    def check_goal(self):
         """
         гол 
         засчитать очко одному или другому
-        вернуть мяч на центр 
-        повернуть мяч в другом направлении
         """
-        pass
+        if self.rect.right >= self.screen_rect.right:
+            print("гол правому")
+            
+            self.throw_in()
+        if self.rect.left <= self.screen_rect.left:
+            print("гол правому")
+            self.throw_in()
 
 
-class Score:
-    """ табло """
-    pass
+class Score(pygame.sprite.Sprite):
+    """
+    табло
+    TODO:
+    метод update, где табло перерисовывает счёт
+    """
+    def __init__(
+            self,
+            color=WHITE,
+            size=72,
+            center=(0, 0),
+            paddle=None,
+    ):
+        super().__init__()
+        self.font = pygame.font.Font(None, size)
+        self.image = self.font.render(str(paddle.score), True, color)
+        self.rect = self.image.get_rect(center=center)
+        self.paddle = paddle
     
 
 game = Game()
